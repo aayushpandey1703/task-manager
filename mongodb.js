b302f42835bff7ffd63e85ad001d99842f895780
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2021-12-10 18:30:41 +0530

    update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,45 +1,68 @@
-const mongodb=require('mongodb')
-const mongoClient=mongodb.MongoClient
+/* 
+no need to create database explicitly at mongodb using robo 3t
+node will automatically create it
+*/
+const {MongoClient,ObjectID}=require('mongodb')
+
+// const mongoClient=mongodb.MongoClient
+// const objectID=mongodb.objectID
 
 const connectionURL='mongodb://127.0.0.1:27017'
 const dbname='task-manager'
 
-mongoClient.connect(connectionURL,{'useNewUrlParser':true},(error,client)=>{
+// const id=new ObjectID()
+// console.log(id)
+// console.log(id.getTimestamp())
+
+MongoClient.connect(connectionURL,{'useNewUrlParser':true},(error,client)=>{                           // connecting node to mongo server
     if(error)
         return console.log("something went wrong\n"+error)
     
-    console.log('connection successfull!')
-    const db=client.db(dbname)                                      // database task-manager created
-
-    db.collection('users').insertOne({                              // collection users created
-        name:'aleron',
-        age:21
-    },(error,result)=>{
-        if(error)
-         return console.log('something went wrong')
-
-        console.log(result.ops)
+    const db=client.db(dbname)                                                                       // database task-manager created 
+    
+    db.collection('users').findOne({_id: new ObjectID('61b33db9e3ff3392c033a157')},(err,result)=>{   // find document by id
+        if (err)
+            return console.log('not found')
+        console.log(result)
     })
 
-    db.collection('tasks').insertMany([                            // collection tasks created
-        {
-            description:'create connection',
-            status:true
-        },
-        {
-            description:'create database',
-            status:true
-        },
-        {
-            description:'insert 3 documents',
-            status:false
-        }
-    ],(error,result)=>{
-        if(error)
-            return console.log('collection task failed')
-        console.log(result.ops+'\ndocument inserted')
-
+    db.collection('users').find({age:22}).sort('name',1).toArray((error,document)=>{                               // find method returns cursor which is pointer to document 
+        if(error)                                                                                   // to get documents we used cursor method 'toArray' check mongo doc cursor for ref
+            return console.log(error)
+        console.log(document)
     })
+
+    
+
+// db.collection('users').insertOne({                                                               // collection users created
+    //     name:'aleron',
+    //     age:21
+    // },(error,result)=>{
+    //     if(error)
+    //      return console.log('something went wrong')
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([                                                          // collection tasks created
+    //     {
+    //         description:'create connection',
+    //         status:true
+    //     },
+    //     {
+    //         description:'create database',
+    //         status:true
+    //     },
+    //     {
+    //         description:'insert 3 documents',
+    //         status:false
+    //     }
+    // ],(error,result)=>{
+    //     if(error)
+    //         return console.log('collection task failed')
+    //     console.log(result.ops+'\ndocument inserted')
+
+    // })
 
     // db.collection('users').insertMany([
     //     {
