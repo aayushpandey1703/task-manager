5eeccfb4d27a6065db061a555d496e24f5e5741c
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2021-12-10 15:02:22 +0530

    update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -4,15 +4,14 @@ const mongoClient=mongodb.MongoClient
 const connectionURL='mongodb://127.0.0.1:27017'
 const dbname='task-manager'
 
-console.log('attempting connection..')
 mongoClient.connect(connectionURL,{'useNewUrlParser':true},(error,client)=>{
     if(error)
-        return console.log(error)
+        return console.log("something went wrong\n"+error)
     
     console.log('connection successfull!')
-    const db=client.db(dbname)
+    const db=client.db(dbname)                                      // database task-manager created
 
-    db.collection('users').insertOne({                              // database users created
+    db.collection('users').insertOne({                              // collection users created
         name:'aleron',
         age:21
     },(error,result)=>{
@@ -22,7 +21,7 @@ mongoClient.connect(connectionURL,{'useNewUrlParser':true},(error,client)=>{
         console.log(result.ops)
     })
 
-    db.collection('tasks').insertMany([                            // databse tasks created
+    db.collection('tasks').insertMany([                            // collection tasks created
         {
             description:'create connection',
             status:true
