312d2d425843544a556ebe60b8fce21a6254f264
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2021-12-11 18:28:57 +0530

    update mongodb.js (CRUD complete)

--- mongodb.js
+++ mongodb.js
@@ -19,51 +19,112 @@ MongoClient.connect(connectionURL,{'useNewUrlParser':true},(error,client)=>{    
         return console.log("something went wrong\n"+error)
     
     const db=client.db(dbname)                                                                       // database task-manager created 
-    
-    db.collection('users').findOne({_id: new ObjectID('61b33db9e3ff3392c033a157')},(err,result)=>{   // find document by id
-        if (err)
-            return console.log('not found')
-        console.log(result)
+ 
+    // insert op
+
+    db.collection('users').insertOne({                                                               // collection users created
+        name:'aleron',
+        age:21
+    },(error,result)=>{
+        if(error)
+         return console.log('something went wrong')
+
+        console.log(result.ops)
     })
 
-    db.collection('users').find({age:22}).sort('name',1).toArray((error,document)=>{                               // find method returns cursor which is pointer to document 
-        if(error)                                                                                   // to get documents we used cursor method 'toArray' check mongo doc cursor for ref
-            return console.log(error)
-        console.log(document)
-    })
+    db.collection('tasks').insertMany([                                                          // collection tasks created
+        {
+            description:'create connection',
+            status:true
+        },
+        {
+            description:'create database',
+            status:true
+        },
+        {
+            description:'insert 3 documents',
+            status:false
+        }
+    ],(error,result)=>{
+        if(error)
+            return console.log('collection task failed')
+        console.log(result.ops+'\ndocument inserted')
 
+    })
     
 
-// db.collection('users').insertOne({                                                               // collection users created
-    //     name:'aleron',
-    //     age:21
-    // },(error,result)=>{
-    //     if(error)
-    //      return console.log('something went wrong')
+    // // delete op
 
-    //     console.log(result.ops)
+    // db.collection('users').deleteOne(
+    //     {
+    //         age:{
+    //             $gt:25
+    //         }
+    //     }
+    // ).then((result)=>{
+    //     console.log(result)
+    // }).catch((error)=>{
+    //     console.log(error)
     // })
 
-    // db.collection('tasks').insertMany([                                                          // collection tasks created
+    // db.collection('tasks').deleteMany(
     //     {
-    //         description:'create connection',
-    //         status:true
-    //     },
-    //     {
-    //         description:'create database',
-    //         status:true
-    //     },
-    //     {
-    //         description:'insert 3 documents',
     //         status:false
     //     }
-    // ],(error,result)=>{
-    //     if(error)
-    //         return console.log('collection task failed')
-    //     console.log(result.ops+'\ndocument inserted')
-
+    // ).then((result)=>{
+    //     console.log(result)
+    // }).catch((error)=>{
+    //     console.log(error)
     // })
 
+    // // update op
+
+    // db.collection('users').updateOne(
+    //     {_id: new ObjectID('61b33db9e3ff3392c033a155')},
+    //     {
+    //         $inc:{
+    //             age:1
+    //         },
+            
+    //         $currentDate:{lastModified:true}
+            
+    //     }).then((result)=>{                                                                         // instead of callback we r using promise
+    //         console.log(result)
+    //     }).catch((error)=>{
+    //         console.log(error)
+    //     })
+
+    // db.collection('tasks').updateMany(
+    //     {
+    //         status:false 
+    //     },
+    //     {
+    //         $set:{
+    //             status:true
+    //         }
+            
+    //     }
+    // ).then((result)=>{
+    //     console.log(result)
+    // }).catch((error)=>{
+    //     console.log(error)
+    // })
+
+    // // Read/Query op
+
+    // db.collection('users').findOne({_id: new ObjectID('61b33db9e3ff3392c033a157')},(err,result)=>{   // find document by id
+    //     if (err)
+    //         return console.log('not found')
+    //     console.log(result)
+    // })
+
+    // db.collection('users').find({age:22}).sort('name',1).toArray((error,document)=>{                               // find method returns cursor which is pointer to document 
+    //     if(error)                                                                                   // to get documents we used cursor method 'toArray' check mongo doc cursor for ref
+    //         return console.log(error)
+    //     console.log(document)
+    // })
+
+    
     // db.collection('users').insertMany([
     //     {
     //         name:'aayush',
