9c6ec7ad9072266bbf050180c3f3bae039feb2ba
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2021-12-18 17:57:06 +0530

    update playground (async-await)

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -4,16 +4,16 @@ const task=require('../src/models/task')
 
 // 61b9e24da23c6aace9cb5b7c (use promise chaining)
 
-user.findByIdAndUpdate('61b9e24da23c6aace9cb5b7c',{
-        name:'Aayush'
-}).then((result)=>{
-    console.log(result)
-    return user.countDocuments({name:'aleron'})
-}).then((result)=>{
-    console.log(result)
-}).catch((e)=>{
-    console.log(e)
-})
+// user.findByIdAndUpdate('61b9e24da23c6aace9cb5b7c',{
+//         name:'Aayush'
+// }).then((result)=>{
+//     console.log(result)
+//     return user.countDocuments({name:'aleron'})
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
 
 // user.countDocuments({age:0}).then((result)=>{
 //     console.log(result)
@@ -41,3 +41,27 @@ user.findByIdAndUpdate('61b9e24da23c6aace9cb5b7c',{
 // }).catch((e)=>{
 //     console.log(e)
 // })
+
+// async-await
+
+// const findAndUpdate=async (id)=>{
+//     const user1=await user.findById(id)
+//     const count=await user.countDocuments()
+//     return count
+// }
+
+// findAndUpdate('61b9e24da23c6aace9cb5b7c').then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+// challenge (async-await)
+
+task.findByIdAndDelete('61bdc9e7d041d5672df40f20').then(async (result)=>{
+    console.log(result)
+    const count=await task.countDocuments({status:false})
+    console.log(count)
+}).catch((e)=>{
+    console.log(e)
+})
--- src/index.js
+++ src/index.js
@@ -7,70 +7,125 @@ const app=express()
 const port=process.env.PORT || 3000
 app.use(express.json())                                             // parse json client request into object
 
-app.post('/users',(req,res)=>{
-    const first=new users(req.body)                                 
+app.post('/users',async (req,res)=>{
+    const first=new users(req.body)       
+    
+    // same response using async-await
+    try{
+        await first.save()
+        res.send(first)
+    }
+    catch(e){
+        res.status(400).send(e)
+    }
 
-    first.save().then((result)=>{
-        res.status(201).send(result)
-    }).catch((error)=>{                                             // handle error in inserting data to server
-    res.status(400).send(error)                                     // to get list of http status code visit https://httpstatuses.com/
-    })
+    // response using promise chaining
+
+    // first.save().then((result)=>{
+    //     res.status(201).send(result)
+    // }).catch((error)=>{                                             // handle error in inserting data to server
+    // res.status(400).send(error)                                     // to get list of http status code visit https://httpstatuses.com/
+    // })
 
 })
 
-app.post('/tasks',(req,res)=>{                      //to test post method endpoint use postman app
+app.post('/tasks',async (req,res)=>{                      //to test post method endpoint use postman app
     const firstTask=new task(req.body)
 
-    firstTask.save().then((result)=>{
-        res.status(201).send(result)                            
-    }).catch((error)=>{
-        res.status(400).send(error)
-    })
+    try{
+        await firstTask.save()
+        res.send(firstTask)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+
+    // firstTask.save().then((result)=>{
+    //     res.status(201).send(result)                            
+    // }).catch((error)=>{
+    //     res.status(400).send(error)
+    // })
 })
 
-app.get('/users',(req,res)=>{
-    users.find({}).then((result)=>{
-        res.send(result)
-    }).catch((error)=>{
-        res.status(500).send(error)
-    })
+app.get('/users',async (req,res)=>{
+
+    try{
+        const user=await users.find()
+        const count=await users.countDocuments()
+        res.send({user:user,docs:count})
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+
+    // users.find({}).then((result)=>{
+    //     res.send(result)
+    // }).catch((error)=>{
+    //     res.status(500).send(error)
+    // })
 })
 
-app.get('/users/:id',(req,res)=>{
+app.get('/users/:id',async (req,res)=>{
     const id=req.params.id
 
-    users.findById(id).then((result)=>{
-        if(result==undefined)
-            return res.status(404).send('not found')
-        res.status(302).send(result)
-    }).catch((error)=>{
-        res.status(500).send(error)
-    })
+    try{
+        const user=await users.findById(id)
+        res.send(user)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+
+    // users.findById(id).then((result)=>{
+    //     if(result==undefined)
+    //         return res.status(404).send('not found')
+    //     res.status(302).send(result)
+    // }).catch((error)=>{
+    //     res.status(500).send(error)
+    // })
 
 })
 
-app.get('/tasks',(req,res)=>{
-    task.find().then((result)=>{
-        if(result.length==0)
-            return res.status(404).send('No task created yet')
-        res.status(302).send(result)
-    }).catch((error)=>{
-        res.status(500).send(error)
-    })
+app.get('/tasks',async (req,res)=>{
+
+    try{
+        const tasks=await task.find()
+        res.send(tasks)
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+
+    // task.find().then((result)=>{
+    //     if(result.length==0)
+    //         return res.status(404).send('No task created yet')
+    //     res.status(302).send(result)
+    // }).catch((error)=>{
+    //     res.status(500).send(error)
+    // })
 })
 
-app.get('/tasks/:id',(req,res)=>{
+app.get('/tasks/:id',async (req,res)=>{
     const id=req.params.id
 
-    task.findById(id).then((result)=>{
-        if(result==undefined)
-            return res.status(404).send("No task found")
-        res.status(302).send(result)
-    }).catch((error)=>{
-        res.send(error)
-    })
+    try{
+        const found=await task.findById(id)
+        res.send(found)
+    }
+    catch(e){
+        res.status(404).send(e)
+    }
+
+    // task.findById(id).then((result)=>{
+    //     if(result==undefined)
+    //         return res.status(404).send("No task found")
+    //     res.status(302).send(result)
+    // }).catch((error)=>{
+    //     res.send(error)
+    // })
 
 })
+
 
 app.listen(port,()=>{
     console.log('server listerning on port',port)
